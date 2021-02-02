2fae2238e08a00b2aa439c381ceac0fc505ece60
Mahadi H Ebrahim <mahadihebrahim@hotmail.com>
2021-02-02 16:32:46 +0600

    undefined vs null

---
+++ undefined-vs-null.js
@@ -0,0 +1,41 @@
+// undefined Example
+
+//example 1
+let pakhi;
+console.log(pakhi);
+
+//example 2
+function sum(num1, num2) {
+    console.log(num1 + num2);
+}
+
+const result = sum(10, 12)
+console.log(result);
+
+//example 3
+function sum(num1, num2) {
+    console.log(num1 + num2);
+    return
+}
+const result = sum(10, 12)
+console.log(result);
+
+//example 4
+function sum(num1, num2) {
+    console.log(num1, num2);
+}
+const result = sum(10)
+console.log(result);
+
+//example 5
+const friend = { id: 12, name: "rahim", phone: 917237 }
+console.log(friend.gf);
+
+// example 6
+let fun = undefined;
+console.log(fun);
+
+
+//null
+let country = null;
+console.log(country);
